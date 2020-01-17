3466f2a38ac0d07da944f91fc6f75b9cda52b6af
dtodt <danieltodt@gmail.com>
2020-01-16 21:25:24 -0300

    feat: audio player working

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,8 +15,30 @@ class XylophoneApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _key(id: 1, color: Colors.red),
+              _key(id: 2, color: Colors.orange),
+              _key(id: 3, color: Colors.yellow),
+              _key(id: 4, color: Colors.green),
+              _key(id: 5, color: Colors.teal),
+              _key(id: 6, color: Colors.blue),
+              _key(id: 7, color: Colors.purple),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _key({int id, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          player.play('sounds/note$id.wav');
+        },
       ),
     );
   }
